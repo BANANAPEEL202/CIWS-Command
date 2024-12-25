8ce34293913e0c40f8310e0f985ce4b77dca8d6c
BANANAPEEL202 <bbob52645@gmail.com>
2024-12-24 22:06:29 -0800

    missile health

---
+++ Assets/GunTurrets2/Demo/Scripts/Missile.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    private BulletPool bulletPool;
+    public GameObject explosionPrefab;
+    public int health = 3;
+
+    private void Awake()
+    {
+        // Get a reference to the BulletPool (assuming it's on the same GameObject or a parent object)
+        bulletPool = FindFirstObjectByType<BulletPool>(); 
+    }
+    
+    private void OnTriggerEnter(Collider collider)
+    {
+        // Check if the collision is with a "Missile"
+        if (collider.gameObject.CompareTag("Bullet"))
+        {
+            bulletPool.ReturnBullet(collider.gameObject); // Return the bullet instead of destroying it
+            // Destroy the missile (or return it to the pool if it has its own pool)
+            if (health > 0)
+            {
+                health--;
+                return;
+            }
+            else {
+                Instantiate(explosionPrefab, transform.position, collider.transform.rotation);
+                Destroy(gameObject); // You can change this to returning to a missile pool if necessary
+            }
+
+        }
+    }
+}
